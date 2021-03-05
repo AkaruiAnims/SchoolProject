6f1cfe894913ba29fcae556592a53e46420bcd05
Jerpharney <jerpharney@gmail.com>
2021-03-05 23:59:44 +0100

    lol

--- Index.php
+++ Index.php
@@ -2,8 +2,33 @@
 <html>
     <head>
         <title>Mall.com</title>
+        <link rel="stylesheet" href="templateStyle.css">
+        <link rel="stylesheet" href="styleSheet.css">
+        <meta charset="UTF-8">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
+            <header>
+                
+            </header>
+            <nav>
 
+            </nav>
+            <main>
+                <div id="I1">
+                    content
+                </div>
+                <div id="I2">
+                    content
+                </div>
+                <div id="I3">
+                    content
+                </div>
+            </main>
+            <footer>
+
+            </footer>
+            <script src="scripts.js"></script>
     </body>
 </html>
